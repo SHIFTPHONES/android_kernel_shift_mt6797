615c4cd28f3d55d9eee763a147381aa17521d158
peixuesong <xuesong.pei@sim.com>
2018-02-09 09:22:10 +0800

    [shift6m] zcv para back 25 degree
    Change-Id: I6b7c4ae7696176d237cc81e75dfa35e3e170eb9e

--- drivers/misc/mediatek/include/mt-plat/mt6797/include/mach/mt_battery_meter.h
+++ drivers/misc/mediatek/include/mt-plat/mt6797/include/mach/mt_battery_meter.h
@@ -41,12 +41,12 @@
 
 /* Qmax for battery  */
 #define Q_MAX_POS_50	2910
-#define Q_MAX_POS_25	4284
+#define Q_MAX_POS_25	2737
 #define Q_MAX_POS_0	1999
 #define Q_MAX_NEG_10	796
 
 #define Q_MAX_POS_50_H_CURRENT	2852
-#define Q_MAX_POS_25_H_CURRENT	4262
+#define Q_MAX_POS_25_H_CURRENT	2682
 #define Q_MAX_POS_0_H_CURRENT	1959
 #define Q_MAX_NEG_10_H_CURRENT	780
 
